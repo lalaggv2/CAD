b45a7da8903bc316195e18c935e12d126c1feac8
L Angela G <luangu2020@gmail.com>
2022-10-15 01:11:10 -0400

    grood struff

---
+++ .anaconda/navigator/scripts/anaconda-navigator-updater.bat
@@ -0,0 +1,3 @@
+chcp 1252
+call C:\ProgramData\Anaconda3\Scripts\activate C:\ProgramData\Anaconda3
+navigator-updater --latest-version 2.2.0 --prefix C:\ProgramData\Anaconda3 >C:\Users\CAD\.anaconda\navigator\scripts\anaconda-navigator-updater-out-2.txt 2>C:\Users\CAD\.anaconda\navigator\scripts\anaconda-navigator-updater-err-2.txt
